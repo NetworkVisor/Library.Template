e9541e352a5154a116aa171b62a30b2ef9211146
Steve Bush <stevebu@bushchang.com>
2021-08-08 23:17:49 -0700

    Removed Revoked PAT. Modified test templates

--- test/core/Library.IntegrationTests/CoreIntegrationTests.cs
+++ test/core/Library.IntegrationTests/CoreIntegrationTests.cs
@@ -1,24 +1,64 @@
-﻿// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+﻿// ***********************************************************************
+// Assembly         : LIBRARY.IntegrationTests
+// Author           : SteveBu
+// Created          : 04-20-2020
+//
+// Last Modified By : SteveBu
+// Last Modified On : 04-20-2020
+// ***********************************************************************
+// <copyright file="CoreIntegrationTests.cs" company="BushChang Corporation">
+//     Copyright (c) BushChang Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace LIBRARY
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace LIBRARY.IntegrationTests
 {
     using LIBRARY;
+    using System;
+    using FluentAssertions;
     using NetworkVisor.Core.Test;
     using NetworkVisor.Core.Test.XUnit;
-    using NetworkVisor.Core.Logging;
+    using NetworkVisor.Core.Test.XUnit.Extensions;
+    using Xunit.Abstractions;
 
-    [PlatformTrait(typeof(CalculatorIntegrationTests))]
-    public class CalculatorIntegrationTests
+    /// <summary>
+    /// Class CoreIntegrationTests.
+    /// </summary>
+    [PlatformTrait(typeof(CoreIntegrationTests))]
+    public class CoreIntegrationTests
     {
-        public CalculatorIntegrationTests()
+        private readonly ITestOutputHelper testOutputHelper;
+        private readonly Type testType = typeof(CoreIntegrationTests);
+
+        public CoreIntegrationTests(ITestOutputHelper testOutputHelper)
         {
+            this.testOutputHelper = testOutputHelper;
         }
 
         [Fact]
-        public void CalculatorIntegration_AddOrSubtract()
+        public void HasPlatformTraitAttribute()
         {
+            this.testType.HasPlatformTraitAttribute().Should().BeTrue();
+        }
 
+        [Fact]
+        public void GetTraitOperatingSystem()
+        {
+            this.testType.GetTraitOperatingSystem().Should().Be(TraitOperatingSystem.Core);
+        }
+
+        [Fact]
+        public void GetTraitTestType()
+        {
+            this.testType.GetTraitTestType().Should().Be(TraitTestType.Integration);
+        }
+
+        [Fact]
+        public void OutputTraits()
+        {
+            this.testOutputHelper.WriteLine($"TestType: {this.testType.GetTraitTestType()}");
+            this.testOutputHelper.WriteLine($"OS Type: {this.testType.GetTraitOperatingSystem()}");
         }
     }
 }
--- test/core/Library.UnitTests/CoreUnitTests.cs
+++ test/core/Library.UnitTests/CoreUnitTests.cs
@@ -1,24 +1,64 @@
-﻿// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+﻿// ***********************************************************************
+// Assembly         : LIBRARY.UnitTests
+// Author           : SteveBu
+// Created          : 04-20-2020
+//
+// Last Modified By : SteveBu
+// Last Modified On : 04-20-2020
+// ***********************************************************************
+// <copyright file="CoreUnitTests.cs" company="BushChang Corporation">
+//     Copyright (c) BushChang Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace LIBRARY
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace LIBRARY.UnitTests
 {
     using LIBRARY;
+    using System;
+    using FluentAssertions;
     using NetworkVisor.Core.Test;
     using NetworkVisor.Core.Test.XUnit;
-    using NetworkVisor.Core.Logging;
+    using NetworkVisor.Core.Test.XUnit.Extensions;
+    using Xunit.Abstractions;
 
-    [PlatformTrait(typeof(CalculatorUnitTests))]
-    public class CalculatorUnitTests
+    /// <summary>
+    /// Class CoreUnitTests.
+    /// </summary>
+    [PlatformTrait(typeof(CoreUnitTests))]
+    public class CoreUnitTests
     {
-        public CalculatorUnitTests()
+        private readonly ITestOutputHelper testOutputHelper;
+        private readonly Type testType = typeof(CoreUnitTests);
+
+        public CoreUnitTests(ITestOutputHelper testOutputHelper)
         {
+            this.testOutputHelper = testOutputHelper;
         }
 
         [Fact]
-        public void CalculatorUnit_AddOrSubtract()
+        public void HasPlatformTraitAttribute()
         {
+            this.testType.HasPlatformTraitAttribute().Should().BeTrue();
+        }
 
+        [Fact]
+        public void GetTraitOperatingSystem()
+        {
+            this.testType.GetTraitOperatingSystem().Should().Be(TraitOperatingSystem.Core);
+        }
+
+        [Fact]
+        public void GetTraitTestType()
+        {
+            this.testType.GetTraitTestType().Should().Be(TraitTestType.Unit);
+        }
+
+        [Fact]
+        public void OutputTraits()
+        {
+            this.testOutputHelper.WriteLine($"TestType: {this.testType.GetTraitTestType()}");
+            this.testOutputHelper.WriteLine($"OS Type: {this.testType.GetTraitOperatingSystem()}");
         }
     }
 }
